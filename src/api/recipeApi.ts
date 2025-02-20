ea27742ef64bb458bf02b659234244813b680d28
Krnsand <krnsandin@gmail.com>
2025-02-20 11:52:48 +0100

    Add Api and api-key

---
+++ src/api/recipeApi.ts
@@ -0,0 +1,19 @@
+// src/api/recipeApi.ts
+const API_KEY = "5bd5c3c84e1647889dedde41889f5bc3"; // Registrera dig på Spoonacular och ersätt denna
+const BASE_URL = "https://api.spoonacular.com/recipes";
+
+export const fetchRecipesByIngredients = async (ingredients: string) => {
+  const response = await fetch(
+    `${BASE_URL}/findByIngredients?ingredients=${ingredients}&number=10&apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data || [];
+};
+
+export const fetchRecipeDetails = async (id: number) => {
+  const response = await fetch(
+    `${BASE_URL}/${id}/information?apiKey=${API_KEY}`
+  );
+  const data = await response.json();
+  return data;
+};
