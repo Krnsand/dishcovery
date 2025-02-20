36edc0dcb044a6448b8e18c3944d9e78b7f420ba
Krnsand <krnsandin@gmail.com>
2025-02-20 14:08:13 +0100

    Get App working

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-
+import Favorites from "./components/Favorites";
+import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
+import RecipePage from "./pages/RecipePage";
 
 const App = () => {
   return (
     <div>
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/recipe/:id" element={<RecipePage />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </div>
   );
