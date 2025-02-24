270468d53542c38770b416b772f3a27be57b0db5
Krnsand <krnsandin@gmail.com>
2025-02-24 14:16:23 +0100

    Add styling to RecipeCard

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,4 +1,31 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Card = styled.div`
+  background: white;
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  transition: transform 0.2s ease-in-out;
+  text-align: center;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const RecipeImage = styled.img`
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  border-radius: 10px;
+`;
+
+const RecipeTitle = styled.h3`
+  font-size: 1.2rem;
+  margin-top: 10px;
+  color: ${(props) => props.theme.colors.primary};
+`;
 
 interface RecipeCardProps {
   recipe: { id: number; title: string; image: string };
