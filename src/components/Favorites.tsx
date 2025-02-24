6316c9a4f96a3331eb76d7879f1098d6a698eb68
Krnsand <krnsandin@gmail.com>
2025-02-24 13:37:12 +0100

    Center style favorites page

--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,4 +1,29 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  background-color: ${(props) => props.theme.colors.background};
+  min-height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5rem;
+  color: ${(props) => props.theme.colors.primary};
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const ContentWrapper = styled.div`
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const Favorites = () => {
   const [favorites, setFavorites] = useState<string[]>([]);
@@ -11,18 +36,20 @@ const Favorites = () => {
   }, []);
 
   return (
-    <div>
-      <h1>⭐ My Favorites</h1>
-      {favorites.length === 0 ? (
-        <p>You have not saved any favorites</p>
-      ) : (
-        <ul>
-          {favorites.map((title) => (
-            <li key={title}>{title}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <Container>
+      <Title>⭐ My Favorites</Title>
+      <ContentWrapper>
+        {favorites.length === 0 ? (
+          <p>You have not saved any favorites</p>
+        ) : (
+          <ul>
+            {favorites.map((title) => (
+              <li key={title}>{title}</li>
+            ))}
+          </ul>
+        )}
+      </ContentWrapper>
+    </Container>
   );
 };
 
