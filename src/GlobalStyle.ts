a6fff8f3aff495994c0957c00d8bbc471924d6b1
Krnsand <krnsandin@gmail.com>
2025-02-26 16:59:11 +0100

    Add gradient to background, maybe like it?

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    background: ${(props) => props.theme.colors.background};
+    background: ${(props) => props.theme.colors.gradient};
     margin: 0;
     padding: 0;
     font-family: Poiret One, Jacques Francois Shadow, Borel, sans-serif;
--- src/theme.ts
+++ src/theme.ts
@@ -4,5 +4,6 @@ export const theme = {
       secondary: "#c6c1ea",
       text: "#333",
       background: "#fcfafc",
+      gradient: "linear-gradient(to bottom, #c6c1ea 1%, #fcfafc 100%);",
     },
   };
