341f7002db391ff0554fa531b6bd6224d6b39f8e
Krnsand <krnsandin@gmail.com>
2025-02-26 11:29:29 +0100

    Get nav links to be centered

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,25 +4,37 @@ import logo from "../assets/images/logo.png";
 
 const Nav = styled.nav`
   display: flex;
-  gap: 15px;
+  align-items: center;
+  justify-content: space-between; /* Sprider ut elementen */
   background-color: ${(props) => props.theme.colors.secondary};
-  padding: 20px;
+  padding: 15px;
   color: ${(props) => props.theme.colors.background};
 `;
 
-const StyledLink = styled(Link)`
+const LinksContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  gap: 50px;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+
+  @media (max-width: 768px) {
+    position: static;
+    transform: none;
+    justify-content: center;
+    width: 100%;
+  }
+`;
+
+const StyledLink = styled(Link)`
   color: ${(props) => props.theme.colors.primary};
   text-decoration: none;
   font-weight: bold;
   font-size: 30px;
+  font-family: "Jacques Francois Shadow";
 `;
 
 const Logo = styled.img`
-  display: flex;
-  justify-content: flex-start;
   height: 120px;
   width: auto;
   margin-right: auto;
@@ -34,8 +46,10 @@ const Navigation = () => {
       <Link to="/">
         <Logo src={logo} alt="Dishcovery Logo" />
       </Link>
-      <StyledLink to="/">Home</StyledLink>
-      <StyledLink to="/favorites">Favorites</StyledLink>
+      <LinksContainer>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/favorites">Favorites</StyledLink>
+      </LinksContainer>
     </Nav>
   );
 };
